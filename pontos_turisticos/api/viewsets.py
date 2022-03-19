a77feb4e42854894a1f526552e32d5aa0a55e00a
lechaves <le.chaves.silva@gmail.com>
2022-03-19 12:04:31 -0300

    implementacao imagens

--- pontos_turisticos/api/viewsets.py
+++ pontos_turisticos/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from ..models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -14,9 +15,35 @@ class PontoTuristicoViewSet(ModelViewSet):
     
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
-    
+        
     def list(self, request, *args, **kwargs):
-        return Response({'test': 123})
+        queryset_base = self.filter_queryset(self.get_queryset())
+        if 'nome' in request.data:
+            queryset = queryset_base.filter(nome__icontains=request.data['nome'])
+        else:
+            queryset = queryset_base.filter()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
-        return Response({'Hello': request.data['nome']})
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    @action(methods=['post'], detail=True)
+    def denunciar(self, request, pk=None):
+        return Response({'Ponto turistico denunciado': True})
+    
+    @action(methods=['get'], detail=False)
+    def situacao(self, request, pk=None):
+        return Response({'Situação dos pontos turisticos': 'OK'})
